e435a6b736c922c405f4f8146c547f5313dac77b
Thomas W <126Twalsh@frhsd.com>
2025-01-15 13:31:49 -0500

    For ms :)

--- js/java.js
+++ js/java.js
@@ -1,1 +1,9 @@
-document 
+$(".Thick").hover(function(){
+    $("#overview").attr("src", "img/BasicFries.jpg");
+  });
+$(".Thin").hover(function(){
+    $("#overview").attr("src", "img/LukeFries");
+  });
+$(".Steak").hover(function(){
+    $("#overview").attr("src", "img/MegalFries.avif");
+  });
