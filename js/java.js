b10cef1045dc48585a0a13641355c84d8cfb1c06
Thomas W <126Twalsh@frhsd.com>
2025-01-15 13:54:16 -0500

    Done :p

--- js/java.js
+++ js/java.js
@@ -1,8 +1,8 @@
 $(".Thick").hover(function(){
-    $(".overview").attr("src","img/BasicFries.jpg");
+    $(".overview").attr("src","img/LukeFries.jpg");
   });
 $(".Thin").hover(function(){
-    $(".overview").attr("src","img/LukeFries");
+    $(".overview").attr("src","img/BasicFries.jpg");
   });
 $(".Steak").hover(function(){
     $(".overview").attr("src","img/MegalFries.avif");
